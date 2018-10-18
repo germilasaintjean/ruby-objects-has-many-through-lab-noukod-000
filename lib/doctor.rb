9392d1b5aebd6983b1583db92d5ce338c89f4d41
Saint-Jean Germila <germila@noukod.com>
2018-10-18 14:45:05 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,32 +1,24 @@
 class Doctor
-
   attr_reader :name, :appointment, :patient
-
-@@all_doctors = []
-
+  @@all_doc = []
   def initialize(name)
     @name = name
-   @@all_doctors << self
-end
-
-  def self.all
-   @@all_doctors
+    @@all_doc << self
   end
-
-def new_appointment (date, patient)
-Appointment.new(date, self, patient)
-#self.appointments.last
-end
-
-def appointments
-  appointment.all.select do |appointment|
-    #appointment.doctor == self
+  def self.all
+    @@all_doc
+  end
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
     end
-end
-
- def patients
-  appointments.map do |appointment|
-   appointment.patients
-   end
- end
+  end
+  def patients
+    appointments.map do |appointment|
+      appointment.patients
+    end
+  end
 end
